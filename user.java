f6573a3b2e3b144c0817b47cbad9da9f81352b0c
Renuka-ux <t.renukasai@gmail.com>
2024-09-21 19:09:07 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,26 @@
+package com.college.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String name;
+    private String email;
+    private String phone;
+}
